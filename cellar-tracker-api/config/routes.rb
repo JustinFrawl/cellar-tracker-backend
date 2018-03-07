c7a84600ff92fe553d0a96af72046398cd5fb6b6
github username <github email address>
2018-03-07 13:33:23 -0500

    setting up JWT auth

--- cellar-tracker-api/config/routes.rb
+++ cellar-tracker-api/config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :cellars
   resources :beers
   resources :users
+  post '/login', to: 'sessions#create'
+  get '/current_user', to: 'sessions#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
