aaf06ba836464d4e357889f0f55e922ba65c52a3
github username <github email address>
2018-03-28 19:23:05 -0400

    still working on auth, password digest

--- cellar-tracker-api/db/schema.rb
+++ cellar-tracker-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315013223) do
+ActiveRecord::Schema.define(version: 20180328231147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,10 +55,11 @@ ActiveRecord::Schema.define(version: 20180315013223) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "user_name"
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
--- cellar-tracker-api/db/seeds.rb
+++ cellar-tracker-api/db/seeds.rb
@@ -5,6 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create( id: 1, name: 'Justin Frawley', username: 'frawlcity', password: 'kenneth')
+User.create( id: 1, name: 'Justin Frawley', user_name: 'frawlcity', password: 'kenneth')
 Beer.create( id: 1, name: '3Frawls', description: 'Inspired by 3 Floyds when I went out there and visited.  Such a balanced malt forward IPA with English accents.', abv: 7, style: 'India Pale Ale', brewery: 'Captain Lawrence' )
 Cellar.create( userId: 1, beerId: 1, rating: 5 )
